e8a4e2bdc2a45d66e97d7799519e6e0a48b10fc6
Tulio Monteiro <tuliocmont@gmail.com>
2022-06-21 17:43:29 -0300

    Price range objects

---
+++ price_range_objects.js
@@ -0,0 +1,5 @@
+const priceRange = [
+  { label: "$", tooltip: "Inexpensive", minPrice: 0, maxPrice: 50 },
+  { label: "$$", tooltip: "Moderate", minPrice: 51, maxPrice: 100 },
+  { label: "$$$", tooltip: "Expensive", minPrice: 101, maxPrice: 999 },
+];
