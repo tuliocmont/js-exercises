662847df60cbe3b6ced39b3a17dcbbbb8064f99e
Tulio Monteiro <tuliocmont@gmail.com>
2022-06-21 17:28:13 -0300

    Nested objects

---
+++ blog_post_object.js
@@ -0,0 +1,13 @@
+const blogPost = {
+  title: "First post",
+  body: "This is my first post",
+  author: "Tulio",
+  views: 777,
+  comments: [
+    { author: "Mateus", body: "Dope bro!" },
+    { author: "Mosh", body: "Keep going!" },
+  ],
+  isLive: true,
+};
+
+console.log(blogPost);
